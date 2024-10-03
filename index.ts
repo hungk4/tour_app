4278b97c18496e5d24e71b68d3a85e899167d232
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-10-03 18:29:48 +0700

    ket noi database

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express, { Express, Request, Response } from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
+import sequelize from "./config/database";
+sequelize;
 const app: Express = express();
-const port: number = 3000;
+const port: number | string  = process.env.PORT ||  3000;
 
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'pug');
