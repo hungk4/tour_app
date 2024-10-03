208b1302fb4bbfd9ec60106b127079a886817f43
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-10-03 17:51:52 +0700

    tao route co ban va cai dat pug

--- index.ts
+++ index.ts
@@ -3,8 +3,17 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
 
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'pug');
+
+
 app.get("/", (req: Request, res: Response) => {
   res.send("trang chu");
+});
+
+
+app.get("/tours", (req: Request, res: Response) => {
+  res.render("client/pages/tours/index");
 });
 
 app.listen(port, () => {
