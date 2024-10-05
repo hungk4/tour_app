20f3f6fcf9edc638db6060f38f545706a8631561
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-10-05 17:29:01 +0700

    lay danh sach tour theo categorie

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -3,7 +3,8 @@ const router = express.Router();
 
 import * as controller from "../../controllers/client/tour.controller";
 
-router.get("/", controller.index);
+router.get("/:slugCategory", controller.index);
+
 
 
 export const tourRoute = router;
