7327311646fd4c3d021dbbc57576411281de168d
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-10-07 15:06:48 +0700

    them gio hang vao local storage

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,41 @@ if(tourImages) {
   });
 }
 // End tour-images
+
+
+// Gio hang
+const cart = localStorage.getItem("cart");
+if(!cart){
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+const fromAddToCart = document.querySelector("[form-add-to-cart]");
+if(fromAddToCart){
+  fromAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = fromAddToCart.querySelector("input[name='quantity']");
+
+    const tourId = parseInt(fromAddToCart.getAttribute("tour-id"));
+    const value = parseInt(input.value);
+
+    if(tourId && value > 0){
+      const cartJS = JSON.parse(localStorage.getItem("cart"));
+  
+      const existTour = cartJS.find((element) => {
+        return element.tourId == tourId
+      })
+  
+      if(existTour){
+        existTour.quantity = existTour.quantity + value;
+      } else {
+        cartJS.push({
+          tourId: tourId,
+          quantity: value
+        })
+      }
+      
+      localStorage.setItem("cart", JSON.stringify(cartJS));
+    }
+    
+  })
+}
+// End Gio hang
