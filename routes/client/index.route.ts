ba8acc4d9cd292206f9747b10cabb96270a86ebe
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-10-07 16:13:28 +0700

    lam giao dien trang cart

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,9 +2,11 @@ import { Express } from "express";
 import { tourRoute } from "./tour.route";
 import { homeRoute } from "./home.route";
 import { categoryRoute } from "./category.route";
+import { cartRoute } from "./cart.route";
 
 export const clientRoute = (app: Express) => {
   app.use("/", homeRoute);
   app.use("/tours", tourRoute);
   app.use("/categories", categoryRoute);
+  app.use("/cart", cartRoute);
 }
