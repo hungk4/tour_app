4e608b904fb3d29d698793c5d1427be6eeb9b6e4
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-10-12 13:19:34 +0700

    them tour co ban

--- index.ts
+++ index.ts
@@ -19,6 +19,9 @@ app.set('view engine', 'pug');
 
 app.use(express.static(`${__dirname}/public`))
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
 app.use(bodyParser.json());
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
