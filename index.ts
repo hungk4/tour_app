47873580e76ae25caaad9256ed1bd5047f88e406
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-10-12 14:31:46 +0700

    infotour bang tiny mce

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import bodyParser from "body-parser";
 import { clientRoute } from "./routes/client/index.route";
 import { adminRoutes } from "./routes/admin/index.route";
 import { systemConfig } from "./config/system";
-
+import path from "path";
 const app: Express = express();
 const port: number | string  = process.env.PORT ||  3000;
 
@@ -17,7 +17,11 @@ app.set('views', `${__dirname}/views`)
 app.set('view engine', 'pug');
 
 
+
+
 app.use(express.static(`${__dirname}/public`))
+
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
